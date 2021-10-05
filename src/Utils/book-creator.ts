0347659de82438dcc5402ec97614ef3aa88f370e
Leonel Ramos <lramos.terp@gmail.com>
2021-10-05 03:46:14 -0400

    refactored to make proper use of async

--- src/Utils/book-creator.ts
+++ src/Utils/book-creator.ts
@@ -3,37 +3,27 @@ import path from 'path';
 import { BookModel } from '../Models/BookModel';
 import supportedFileTypes from '../Shared/supported-file-types';
 
-export function getBooksAsync(url: string) {
+export async function getBooksAsync(url: string) {
   const absolutePath = path.resolve(url);
 
-  console.log(`Getting books from ${absolutePath}`);
-
-  return readdir(absolutePath)
-    .then(async (files) => {
-      const books = await createBooksAsync(absolutePath, files);
-      console.log(`Final book list: ${books}`);
-      return books;
-    })
-    .catch((err) => {
-      console.log(
-        `Error when trying to read from directory ${absolutePath} - ${err}`
-      );
-      return [] as BookModel[];
-    });
+  const files = await readdir(absolutePath);
+  const books = await createBooksAsync(absolutePath, files);
+  return books as Promise<BookModel[]>;
 }
 
 async function createBooksAsync(absolutePath: string, files: string[]) {
   const dir = path.resolve(absolutePath);
-  const books: BookModel[] = [];
 
-  files.forEach((file) => {
-    const filepath = path.join(dir, file);
+  return new Promise((resolve) => {
+    const books: BookModel[] = [];
 
-    if (supportedFileTypes.includes(path.extname(file))) {
-      console.log(`${file} added!`);
-      books.push(new BookModel(filepath));
-    }
+    files.forEach((file) => {
+      const filepath = path.join(dir, file);
+      const isSupportFileType = supportedFileTypes.includes(path.extname(file));
+      if (isSupportFileType) {
+        books.push(new BookModel(filepath));
+      }
+    });
+    resolve(books);
   });
-
-  return books;
 }
