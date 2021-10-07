3c54ef471685228d0eed5cd39caf353c9bf802ab
Leonel Ramos <lramos.terp@gmail.com>
2021-10-07 15:25:14 -0400

    moved location of current epub renderer

---
+++ src/preload/scripts/book/renderers/book-renderer.ts
@@ -0,0 +1,21 @@
+import epub from 'epubjs'
+import { RenditionOptions } from 'epubjs/types/rendition';
+// TODO: Move book creation to own function
+
+export default function (url: string) {
+    const renderArea = document.getElementById('book-render-area');
+    if (renderArea) {
+        const book = epub(url);
+        const renditionOptions: RenditionOptions = {
+            manager: "continuous",
+            flow: "scrolled",
+            width: '100%',
+            height: '100%'
+        }
+        const rendition = book.renderTo(renderArea, renditionOptions);
+        const displayed = rendition.display();
+    } else {
+        throw (`Error! Render area is null!`);
+    }
+}
+
