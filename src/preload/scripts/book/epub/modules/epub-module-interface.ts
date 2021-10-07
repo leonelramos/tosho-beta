f3eae986f14abfd9b4b3a1cc239882910ac65e84
Leonel Ramos <lramos.terp@gmail.com>
2021-10-07 15:24:07 -0400

    added interface for epub modules

---
+++ src/preload/scripts/book/epub/modules/epub-module-interface.ts
@@ -0,0 +1,17 @@
+export default interface IEpubModule {
+    initAsync(url: string): Promise<void>,
+    getCoverUrl(): string,
+    getMetadata(): EpubMetadata,
+    getImageElement(): HTMLImageElement,
+}
+
+export interface EpubMetadata {
+    id: string,
+    title: string,
+    language: string,
+    creator?: string,
+    edition?: string,
+    description?: string,
+    publisher?: string,
+    publishedDate?: string
+}
