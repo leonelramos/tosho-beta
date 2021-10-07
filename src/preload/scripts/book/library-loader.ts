2b726e954f35b5ba6063c35826261a25303200b7
Leonel Ramos <lramos.terp@gmail.com>
2021-10-07 15:27:33 -0400

    abstracted out creation of book models
    necessary change to make multiple formats possible in the future

---
+++ src/preload/scripts/book/library-loader.ts
@@ -0,0 +1,48 @@
+import path from 'path';
+import JSZip from 'jszip';
+import Epub, { Book } from 'epubjs';
+import fs, { readdir } from 'fs/promises';
+import supportedFileTypes from '@/shared/scripts/currently-supported-file-types';
+import { BookModel } from '@/shared/models/book';
+import defaultCoverUrl from '@/assets/img/defaultcover.jpg';
+import { FileTypes } from '@/preload/scripts/book/file-types-enum';
+import { EpubCreator } from '@/preload/scripts/book/creators/epub-creator';
+
+export async function getBooksAsync(url: string): Promise<BookModel[]> {
+  const files = await readdir(url);
+  console.log("files: ", files);
+  return await createBooksAsync(url, files);
+}
+
+async function createBooksAsync(absolutePath: string, files: string[]): Promise<BookModel[]> {
+  const supportedFiles = files.filter(file => {
+    const ext = path.extname(file);
+    const fileTypeSupported = supportedFileTypes.includes(ext);
+    return fileTypeSupported;
+  });
+
+  const booksSettled = await Promise.allSettled(supportedFiles.map(file => {
+    const pathName = path.join(absolutePath, file);
+    const ext = path.extname(file);
+    return createBookAsync(pathName, ext);
+  }));
+
+  let books: BookModel[] = [];
+  booksSettled.forEach(book => {
+    book.status === 'fulfilled' && books.push(book.value);
+  });
+
+  return books;
+}
+
+async function createBookAsync(url: string, ext: string): Promise<BookModel> {
+  switch (ext) {
+    case FileTypes.EPUB:
+      const epubCreator = new EpubCreator();
+      return epubCreator.createBookAsync(url);
+      break;
+    default:
+      return Promise.reject("Book format not currently supported.");
+  }
+}
+
