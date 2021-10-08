b6013698b7af6cf1608c0149e6f9685f84608611
Leonel Ramos <lramos.terp@gmail.com>
2021-10-08 19:03:09 -0400

    added types for contextBridge api

---
+++ src/renderer/types/renderer.d.ts
@@ -0,0 +1,29 @@
+import { BookModel } from "@/shared/models/book";
+
+export interface IPathApi {
+  resolve(url: string, ...pathArgs: string[]): string;
+  join(url: string, ...pathArgs: string[]): string;
+  rendererPath: string;
+}
+
+export interface IEnvApi {
+  isDevelopment: boolean;
+}
+
+export interface IBookApi {
+  render(url: string): void;
+  importFolder(url: string): Promise<BookModel[]>
+}
+
+export interface ISystemApi {
+  getDialogFolderUrl(): Promise<{ canceled: boolean, filePaths: string[], bookmarks?: string[] }>;
+}
+
+declare global {
+  interface Window {
+    pathApi: IPathApi,
+    envApi: IEnvApi,
+    bookApi: IBookApi,
+    systemApi: ISystemApi
+  }
+}
