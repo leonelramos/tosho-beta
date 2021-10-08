da9f3792810ff82da7542f8be993c9f595c8b4d0
Leonel Ramos <lramos.terp@gmail.com>
2021-10-07 22:18:55 -0400

    added styles for menu

---
+++ src/shared/styles/chakra-theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react"
+
+const config: ThemeConfig = {
+  useSystemColorMode: true,
+}
+
+const theme = extendTheme({ config })
+export default theme
